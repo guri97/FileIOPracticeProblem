2454cacf6702869e644d63f6aa9b94f6ef1d7840
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-05 22:06:46 +0530

    [gurpreet] UC1_CheckFileExistorNot

---
+++ FileIOPracticeProblem/FileIOPracticeProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace FileIO
+
+{
+    class Program
+    {
+        public static void FileExist()
+        {
+            string InputFile = @"C:\Users\GURPREET SINGH\Desktop\VisualStudio\FileIOTextFile";
+            if (File.Exists(InputFile))
+            {
+                Console.WriteLine("File Exist");
+            }
+            else
+            {
+                Console.WriteLine("not Exist");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+
+            FileExist();
+
+        }
+    }
+}
